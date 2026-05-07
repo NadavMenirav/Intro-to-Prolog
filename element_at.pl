c6b69d66204e527462f56045ad57d1d90e285b96
NadavMenirav <nmenirav@gmail.com>
2026-05-07 19:24:00 +0300

    (Feat) removes an element from a list

--- element_at.pl
+++ element_at.pl
@@ -1,3 +1,3 @@
 % This funciton returns the element of the list in the required index
 element_at(H, [H|_], 1).
-element_at(X, [_|T], K) :- K1 is K - 1, element_at(X, T, K1).
+element_at(X, [_|T], K) :- K > 1, K1 is K - 1, element_at(X, T, K1).
